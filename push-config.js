1efce9cc4593148d59eea647433d28c0b7e064fd
phanluanvt <phanluanvt@gmail.com>
2026-08-28 08:38:48 -0700

    Add OneSignal public app configuration placeholder

---
+++ push-config.js
@@ -0,0 +1,3 @@
+// OneSignal App ID is public and safe to expose in client-side code.
+// Replace the empty value after creating the JobsOther app in OneSignal.
+window.JOBSOTHER_ONESIGNAL_APP_ID = "";
